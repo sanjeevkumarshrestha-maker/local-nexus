e0c219497cf5cea40535169082ef45a5efe21d53
unknown <sanjeevkumarshrestha@gmail.comm>
2026-09-04 09:46:05 +0545

    Add DentistsGrid schema

--- src/collections/Pages.ts
+++ src/collections/Pages.ts
@@ -1,7 +1,6 @@
 import type { CollectionConfig } from 'payload'
 import { DetailedServicesBlock } from '../blocks/DetailedServices'
-import { HeroBlock, StoryBlock, CredibilityBlock, ServicesHighlight, WhyChooseUsBlock, DentistsGridBlock,  } from '../blocks/Registry'
-
+import { HeroBlock, StoryBlock, CredibilityBlock, ServicesHighlight, WhyChooseUsBlock, DentistsGridBlock } from '../blocks/Registry'
 
 export const Pages: CollectionConfig = {
   slug: 'pages',
@@ -26,6 +25,7 @@ export const Pages: CollectionConfig = {
         CredibilityBlock,
         ServicesHighlight,
         WhyChooseUsBlock,
+        DentistsGridBlock,
       ],
     },
   ],
